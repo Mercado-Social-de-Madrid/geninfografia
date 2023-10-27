74720b7d0191e294ea09ebc0b749f9310c2a6309
julio <jberzal86@gmail.com>
2023-10-27 15:01:14 +0200

    - Generating string json files from csv

---
+++ translations/gen_jsons.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import json
+
+df = pd.read_csv('strings.csv')
+
+def gen_json(column_name, output_filename):
+
+    data = dict(zip(df['Código'], df[column_name]))
+
+    with open(output_filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+gen_json('Castellano', 'cas.json')
+gen_json('Català', 'cat.json')
+gen_json('Euskera', 'eus.json')
+gen_json('Galego', 'gal.json')

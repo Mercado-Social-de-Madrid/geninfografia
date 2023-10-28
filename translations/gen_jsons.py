db2fc9acaef3347a3e4be72529eed36685e70492
Alberto Moreta <albertomoreta70@gmail.com>
2023-10-28 12:49:51 +0200

    ﻿Update translations

--- translations/gen_jsons.py
+++ translations/gen_jsons.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import json
 
-df = pd.read_csv('strings.csv')
+df = pd.read_csv("strings.csv")
 
 def gen_json(column_name, output_filename):
 
-    data = dict(zip(df['Código'], df[column_name]))
+    data = dict(zip(df['Código'], df[column_name].str.strip()))
 
     with open(output_filename, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
